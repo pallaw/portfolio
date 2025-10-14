ed3fa5398323bff2039ee7a0004f891a7ea58769
Pallaw Pathak <pallaw.pathak@gmail.com>
2025-10-14 09:42:53 +0000

    auto-commit for 88c03d72-8f1d-421b-baa0-e187ec5b8d9e

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import React, { useEffect } from 'react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { md3Theme } from './theme/md3Theme';
 import './App.css';
 import './styles/fonts.css';
 import Header from './components/Header';
-import HeroMD3 from './components/HeroMD3';
-import AboutMD3 from './components/AboutMD3';
+import Hero from './components/Hero';
+import About from './components/About';
 import Experience from './components/Experience';
-import SkillsMD3 from './components/SkillsMD3';
+import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Blog from './components/Blog';
 import Contact from './components/Contact';
